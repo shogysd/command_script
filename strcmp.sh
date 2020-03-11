d48c62a45dcb3a6304a97b3aa8826c16d41d4aaf
Shogo Yasuda <shog.ysd@gmail.com>
2020-03-12 00:43:34 +0900

    add strcmp.sh

---
+++ strcmp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# = 0 ]; then
+    echo "error: nothing arguments"
+elif [ $# = 1 ]; then
+    echo "error: inadequate arguments"
+elif [ $# -gt 2 ]; then
+    echo "error: many arguments"
+else
+    if [ "$1" = "$2" ]; then
+        echo "True"
+    else
+        echo "False"
+    fi
+fi
